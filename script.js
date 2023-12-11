7810c25690067c53859f58345cb5b2b44ecdc418
zwei9lake <a9subiyanto@gmail.com>
2023-12-11 22:30:08 +0700

    add thanks container and implement javascript

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-function score() {
-  document.getElementsByClassName("score").style.backgroundColor = "white";
-}
+const ratingContainer = document.querySelector(".submit-rating");
+const thanksContainer = document.querySelector(".thank-you");
+const submitButton = document.getElementById("submit");
+const rating = document.getElementById("rating");
+const rates = document.querySelectorAll(".score");
+
+submitButton.addEventListener("click", () => {
+  thanksContainer.classList.remove("hidden");
+  ratingContainer.style.display = "none";
+});
+
+rates.forEach((rate) => {
+  rate.addEventListener("click", () => {
+    rating.innerHTML = rate.innerHTML;
+  });
+});
